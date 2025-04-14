31e4f2d2cfc45d6a6638465338c2beb7c017f6b7
Tassi <tassi.tbj@gmail.com>
2025-04-14 00:11:26 -0300

    Parte 3.5 agregado de atajo ger_object_or_404 vista mejorada

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 
 def index(request):
@@ -25,9 +25,7 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("No existe esa pregunta")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
