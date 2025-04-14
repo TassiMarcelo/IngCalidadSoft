4a433a4510b725200f99f3e0b710c1fafd70bdac
Tassi <tassi.tbj@gmail.com>
2025-04-13 23:52:09 -0300

    Parte 3.2 actualizacion de vistas, creacion de index.html en polls/templates/polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 
+from django.template import loader
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
     return HttpResponse("Hola , se creo la vista !!!!")
@@ -17,3 +17,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
