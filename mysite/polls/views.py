bf15dfcea5d8f87f227be21facd5b49145c337c9
Tassi <tassi.tbj@gmail.com>
2025-04-14 00:03:47 -0300

    Parte 3.4 se agrega una excepcion 404 si no existe una pregunta habiendo indicado su id

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
 
-from django.template import loader
 from django.http import HttpResponse
 from .models import Question
 from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
     return HttpResponse("Hola , se creo la vista !!!!")
@@ -23,3 +23,11 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("No existe esa pregunta")
+    return render(request, "polls/detail.html", {"question": question})
